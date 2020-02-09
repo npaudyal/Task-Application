735a181bdd0176fe73c2af30db8476e7db9d115d
Nischal Paudyal <zlatan.nis111@gmail.com>
2020-02-09 15:17:24 -0600

    =Fixed user authentication

--- src/index.js
+++ src/index.js
@@ -6,26 +6,23 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.json())
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
 
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is currently down. Check back soon!')
+// })
+
+app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const bcrypt = require('bcryptjs')
-
-const myFunction = async() =>{
-    const password = "Red12345!"
-    const hashedP = await bcrypt.hash(password, 8)
-    console.log(password)
-    console.log(hashedP)
-
-    const isMatch = await bcrypt.compare('red', hashedP)
-    console.log(isMatch)
-}
-
-myFunction()
 
